3ec2700fb7eddba783fcbc8a973cbb2186444e7a
Thybak <davidm.martin@outlook.com>
2023-12-03 21:43:40 +0100

    feat: add new puke functionality to help action

--- src/main/java/com/thybak/bots/kkbot/adapter/inbound/action/KkBotHelpAction.java
+++ src/main/java/com/thybak/bots/kkbot/adapter/inbound/action/KkBotHelpAction.java
@@ -9,12 +9,13 @@ public class KkBotHelpAction implements KkBotAction {
     private static final String HELP_COMMAND = "/help";
     private static final String HELP_COMMAND_TEXT = """
             Hola! Soy tu registrador de kks. Encantado!
-            Lo primero de todo, descuida! Los datos solo me interesan a mí y solo guardo las fechas en que registras que sacó la cabeza la tortuga.
+            Lo primero de todo, descuida! Los datos solo me interesan a mí y solo guardo las fechas en que registras que sacó la cabeza la tortuga, entre otros.
             
             De momento *éstas son mis funcionalidades*:
             
             *Almacenar que hemos pasado por el cagadero*: Escribe simplemente un emoji de una kk y en caso de no poder confirmar el registro te avisaré.
-            *Ranking de kks*: cada lunes y cada día 1 de cada mes calcularé el ranking de kks a las 8 de la mañana para vosotros!""";
+            *Almacenar que hemos gomitao*: Desahógate con nosotros y escribe \uD83E\uDD2E. En caso de no poder confirmar el registro te avisaré.
+            *Ranking de secreciones*: cada lunes y cada día 1 de cada mes calcularé el ranking de kks y potas a las 8 de la mañana para vosotros!""";
 
     @Override
     public String getCommand() {
